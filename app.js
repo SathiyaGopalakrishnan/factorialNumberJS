eac4313eb4b11097d7f3dc0628f09265a0d6157b
SathiyaGopalakrishnan <63980261+SathiyaGopalakrishnan@users.noreply.github.com>
2024-08-08 21:52:14 +1200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+
+
+   // factorial number
+ var j=parseInt(prompt("enter the number you want to find factotial number"));
+ var k=j;
+ var m=j;
+
+while(j>1)
+ {
+        j--;
+k=k*j;
+
+ }
+ document.write("factorial value of "+m+" is :=",+k+"<br>");
+    
+ 
+ 
